beedce3dcaeb1821edcedb4b3e32467025e891cb
chenuvi <chenuvi@outlook.com>
2024-01-25 19:03:35 +0800

    Feat: pipe

---
+++ 7_Stream/08_pipe.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+const MyReadStream = require("./ReadStream");
+
+// const rs = fs.createReadStream("./test.txt", {
+//   highWaterMark: 4,
+// });
+
+// const ws = fs.createWriteStream("./f10.txt", {
+//   highWaterMark: 1,
+// });
+
+// rs.pipe(ws);
+
+const rs = new MyReadStream("./test.txt");
+const ws = fs.createWriteStream("f11.txt");
+rs.pipe(ws);
